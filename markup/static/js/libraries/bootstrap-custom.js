898c1a263e3a0afb4df7d88c8ffdb90f7824fcc8
Narek Sahakyan <nareksahakyan26@gmail.com>
2017-05-22 17:25:41 +0400

    customize tars

---
+++ markup/static/js/libraries/bootstrap-custom.js
@@ -0,0 +1,19 @@
+/*!
+ * Bootstrap v3.3.7 (http://getbootstrap.com)
+ * Copyright 2011-2016 Twitter, Inc.
+ * Licensed under the MIT license
+ */
+
+
+require("bootstrap-sass/assets/javascripts/bootstrap/transition");
+// require("bootstrap-sass/assets/javascripts/bootstrap/alert");
+// require("bootstrap-sass/assets/javascripts/bootstrap/button");
+// require("bootstrap-sass/assets/javascripts/bootstrap/carousel");
+// require("bootstrap-sass/assets/javascripts/bootstrap/collapse");
+// require("bootstrap-sass/assets/javascripts/bootstrap/dropdown");
+require("bootstrap-sass/assets/javascripts/bootstrap/modal");
+// require("bootstrap-sass/assets/javascripts/bootstrap/tab");
+// require("bootstrap-sass/assets/javascripts/bootstrap/affix");
+// require("bootstrap-sass/assets/javascripts/bootstrap/scrollspy");
+require("bootstrap-sass/assets/javascripts/bootstrap/tooltip");
+// require("bootstrap-sass/assets/javascripts/bootstrap/popover");
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import $ from 'jquery';
+import 'static/js/libraries/bootstrap-custom';
+
 /*
     This file can be used as entry point for webpack!
  */
